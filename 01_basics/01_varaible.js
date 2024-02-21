59daf7a525648af27e113940a3e2c00510d029d4
SAURABH TIWARI <115872589+SAURABHTIWARIVBSPU@users.noreply.github.com>
2024-02-21 14:15:40 +0000

    changes the meaning of the folder

---
+++ 01_basics/01_varaible.js
@@ -0,0 +1,15 @@
+const accountId = 145553;
+let accountEmail="stw284701@gmail.com";
+var accountPassword="75953021"
+accountCity="Jaipur";
+//accountId=2;// not allowed
+
+{
+
+    //issi ko scope bola jata hai //
+}
+console.log(accountId);
+console.table([accountId,accountEmail,accountCity]);
+// let accountstate; agr hum kisse bhe variable ko define nhi karte hai to wo
+undefined so karta hai output
+// 
