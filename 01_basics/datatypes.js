0b215f1b2566f440543cc2b24a338ddc3ff858e9
SAURABH TIWARI <115872589+SAURABHTIWARIVBSPU@users.noreply.github.com>
2024-02-24 17:53:35 +0000

    data-types

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,19 @@
+const score=100;
+const scoreValue=100.3;
+const isLoggedIn=false;
+const outsideTeam=null;
+let userEmail;
+const id=Symbol('123');
+const anotherId=Symbol('123');
+console.log(id==anotherId);
+console.log(outsideTeam);
+console.log(userEmail);
+console.log(isLoggedIn);
+const hero=["shaktiman", "nagaraj","Saurabh"]
+let myobj={
+    name: "mohan",
+    age:22
+   
+}
+console.log(myobj);
+console.log(hero);
---
+++ 01_basics/datatypesconversion.js
@@ -0,0 +1,9 @@
+let isLoggedIn=1;
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof isLoggedIn);
+let num=1345678945625556n;
+console.log(num);
+console.log(typeof num);
+let a=1,b=2,c=3,d=4,e=5;
+console.log((a+b)/c-(b-a)*(c+d));
