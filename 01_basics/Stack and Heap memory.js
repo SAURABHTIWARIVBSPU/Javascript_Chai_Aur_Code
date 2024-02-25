4f9eef2d59209b95c9cd76f9fda5c5f522280bbf
SAURABH TIWARI <115872589+SAURABHTIWARIVBSPU@users.noreply.github.com>
2024-02-25 20:08:34 +0000

    stack and heap memory concepts

---
+++ 01_basics/Stack and Heap memory.js
@@ -0,0 +1,18 @@
+let myyoutubename="trickey tech";
+let anothername=myyoutubename;
+anothername="chai aur code";
+console.log(myyoutubename);
+console.log(anothername);
+let userOne={
+    email:"stw@gmail.com",
+    upi:"stw@ybl",
+}
+console.log(userOne);
+userOne=userTwo={
+    email:"tiwari@gmail.com",
+    upi:"tiwari@ybl",
+}
+/* ye first statement ko change kar raha hai user key kyuki isko heap se refference mil raha hai isiliye direct;*/
+
+console.log(userOne);
+console.log(userTwo);
