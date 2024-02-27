a1fa88df2ab6a8e77c8f03002bc0a2864ddbaef8
SAURABH TIWARI <115872589+SAURABHTIWARIVBSPU@users.noreply.github.com>
2024-02-27 19:44:57 +0000

    done

---
+++ 01_basics/String/String.charAt.js
@@ -0,0 +1,6 @@
+const sentence = 'hello how are you .';
+
+const index = 4;
+
+console.log(`The character at index ${index} is ${sentence.charAt(index)}`);
+// Expected output: "The character at index 4 is o"
---
+++ 01_basics/String/String.concat.js
@@ -0,0 +1,7 @@
+const value1="krishna";
+
+const value2="kumar";
+
+//console.log(value1, value2);
+console.log(value1.concat(' ', value2));
+console.log(value2.concat(' ', value1));
